60df3d82ac565a3a61607aa0b67c17959f2a4ad3
Francisco Camacho <fcamachocervantes@mines.edu>
2023-04-10 15:20:15 -0600

    fixed level primary key

--- app/src/main/java/edu/mines/csci448/pcm/blockbrawl/data/BlockBrawlLevel.kt
+++ app/src/main/java/edu/mines/csci448/pcm/blockbrawl/data/BlockBrawlLevel.kt
@@ -1,7 +1,10 @@
 package edu.mines.csci448.pcm.blockbrawl.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity(tableName = "levels")
-data class BlockBrawlLevel(val levelId: UUID)
+data class BlockBrawlLevel(
+                            @PrimaryKey
+                            val levelId: UUID)
