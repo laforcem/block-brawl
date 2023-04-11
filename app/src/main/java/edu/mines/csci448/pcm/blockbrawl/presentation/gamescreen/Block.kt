40638f1d543d6f6d4c14cc178011d273df1a10b2
cOlsgard <90218594+cOlsgard@users.noreply.github.com>
2023-04-10 22:01:52 -0600

    Set up a canvas with a draggable block for the game

---
+++ app/src/main/java/edu/mines/csci448/pcm/blockbrawl/presentation/gamescreen/Block.kt
@@ -0,0 +1,30 @@
+package edu.mines.csci448.pcm.blockbrawl.presentation.gamescreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+
+class Block(
+    var x_pos: Float,
+    var y_pos: Float,
+    val shape: Array<Array<Boolean>>,
+    val color: Color,
+    val canvasWidth: Float,
+    val canvasHeight: Float,
+    val gameboard_size: Int
+    ) {
+    val WIDTH = canvasWidth / gameboard_size
+}
+
+fun DrawScope.drawBlock(block: Block, offsetX: Float, offsetY: Float){
+    drawRect(
+        color = block.color,
+        size = Size(width = block.WIDTH, height = block.WIDTH),
+        topLeft = Offset(block.x_pos + offsetX, block.y_pos + offsetY),
+    )
+}
--- app/src/main/java/edu/mines/csci448/pcm/blockbrawl/presentation/gamescreen/GameScreen.kt
+++ app/src/main/java/edu/mines/csci448/pcm/blockbrawl/presentation/gamescreen/GameScreen.kt
@@ -1,20 +1,15 @@
 package edu.mines.csci448.pcm.blockbrawl.presentation.gamescreen
-
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import edu.mines.csci448.pcm.blockbrawl.R
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import edu.mines.csci448.pcm.blockbrawl.presentation.viewmodel.IBlockBrawlViewModel
 
 @Composable
@@ -23,26 +18,67 @@ fun GameScreen(
     onPauseClicked: () -> Unit
 )
 {
-    Column(
+    val offsetX = remember { mutableStateOf(0f) }
+    val offsetY = remember { mutableStateOf(0f) }
+    val draggingBlock = remember { mutableStateOf(false) }
+    BoxWithConstraints(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
     ){
+        val canvasWidth = LocalDensity.current.run { maxWidth.toPx()}
+        val canvasHeight = LocalDensity.current.run { maxHeight.toPx()}
+        val WIDTH = canvasWidth / 11
 
-        //Game Content Would go here
-        Box(
-            contentAlignment = Alignment.CenterStart,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ){
-            Text(
-                text = "Game Screen Content Would Go Here",
-                fontSize = 50.sp,
-                textAlign = TextAlign.Center,
-                lineHeight = 50.sp
-            )
-        }
+        //TODO: set up a list of blocks in the ViewModel. This is just a temporary test block
+        val block = Block(
+            100f,
+            300f,
+            arrayOf(arrayOf(true)),
+            Color(0xFF6a4db3),
+            canvasWidth,
+            canvasHeight,
+            11
+        )
+
+        //Game content goes here
+            Canvas(modifier = Modifier.fillMaxSize().pointerInput(Unit) {
+                detectDragGestures (
+                    onDragStart = {
+                        //TODO: use the getTappedBlock function to determine what block (if any) was tapped
+                        if (it.x > block.x_pos
+                            && it.x < block.x_pos + WIDTH
+                            && it.y > block.y_pos
+                            && it.y < block.y_pos + WIDTH){
+                                draggingBlock.value = true
+                            }
+                    },
+                    onDragEnd = {
+                        draggingBlock.value = false
+                        block.x_pos = block.x_pos + offsetX.value
+                        block.y_pos = block.y_pos + offsetY.value
+                        offsetX.value = 0f;
+                        offsetY.value = 0f;
+                    })
+                { change, dragAmount ->
+                    if (draggingBlock.value){
+                        change.consume()
+                        offsetX.value += dragAmount.x
+                        offsetY.value += dragAmount.y
+                    }
+                }
+            })
+            {
+
+                drawBlock(block, offsetX.value, offsetY.value);
+            }
     }
+}
+
+fun getTappedBlock(offset: Offset): Block? {
+    //TODO: set up a list of all blocks that have not been placed on
+    //      the board. Then figure out which one was pressed and return
+    //      that block so it can be dragged.
+    return null
 }
 
 //@Preview
